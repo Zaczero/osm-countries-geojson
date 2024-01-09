cb1be427c1ab1a99a7688400b70bf497f57c614b
Kamil Monicz <kamil@monicz.dev>
2024-01-09 14:50:50 +0100

    Python 3.12, Poetry

--- shell.nix
+++ shell.nix
@@ -1,37 +1,42 @@
-{ pkgsnix ? import ./pkgs.nix
-, pkgs ? pkgsnix.pkgs
-, unstable ? pkgsnix.unstable
-, isDocker ? false
-}:
+{ isDevelopment ? true }:
 
-with pkgs; let
-  commonBuildInputs = [
+let
+  # Currently using nixpkgs-23.11-darwin
+  # Get latest hashes from https://status.nixos.org/
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/207b14c6bd1065255e6ecffcfe0c36a7b54f8e48.tar.gz") { };
+
+  libraries' = with pkgs; [
+    # Base libraries
     stdenv.cc.cc.lib
-    python311
+    zlib.out
   ];
 
-  devBuildInputs = [
-    gnumake
-    pipenv
+  packages' = with pkgs; [
+    # Base packages
+    python312
+  ] ++ lib.optionals isDevelopment [
+    # Development packages
+    poetry
     ruff
   ];
 
-  commonShellHook = ''
-  '';
-
-  devShellHook = ''
-    export LD_LIBRARY_PATH="${lib.makeLibraryPath commonBuildInputs}"
-    export PIPENV_VENV_IN_PROJECT=1
-    export PIPENV_VERBOSITY=-1
+  shell' = with pkgs; ''
+    export PROJECT_DIR="$(pwd)"
+  '' + lib.optionalString isDevelopment ''
     [ ! -e .venv/bin/python ] && [ -h .venv/bin/python ] && rm -r .venv
-    [ ! -f .venv/bin/activate ] && pipenv sync --dev
-    case $- in *i*) exec pipenv shell --fancy;; esac
-  '';
 
-  dockerShellHook = ''
+    echo "Installing Python dependencies"
+    export POETRY_VIRTUALENVS_IN_PROJECT=1
+    poetry install --no-root --compile
+
+    echo "Activating Python virtual environment"
+    source .venv/bin/activate
+
+    export LD_LIBRARY_PATH="${lib.makeLibraryPath libraries'}"
   '';
 in
 pkgs.mkShell {
-  buildInputs = commonBuildInputs ++ (if isDocker then [ ] else devBuildInputs);
-  shellHook = commonShellHook + (if isDocker then dockerShellHook else devShellHook);
+  libraries = libraries';
+  buildInputs = libraries' ++ packages';
+  shellHook = shell';
 }
