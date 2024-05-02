090c819d106e481303d48c02df1fb448cab67765
Kamil Monicz <kamil@monicz.dev>
2024-05-02 20:52:52 +0000

    Update packages, Cache dependencies in workflow

--- shell.nix
+++ shell.nix
@@ -1,55 +1,62 @@
-{ isDevelopment ? true }:
-
 let
-  # Currently using nixpkgs-23.11-darwin
+  # Currently using nixpkgs-unstable
   # Update with `nixpkgs-update` command
-  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/1d1817869c47682a6bee85b5b0a6537b6c0fba26.tar.gz") { };
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/bcd44e224fd68ce7d269b4f44d24c2220fd821e7.tar.gz") { };
 
-  libraries' = with pkgs; [
-    # Base libraries
+  pythonLibs = with pkgs; [
     stdenv.cc.cc.lib
     zlib.out
   ];
 
+  # Override LD_LIBRARY_PATH to load Python libraries
+  wrappedPython = with pkgs; symlinkJoin {
+    name = "python";
+    paths = [ python312 ];
+    buildInputs = [ makeWrapper ];
+    postBuild = ''
+      wrapProgram "$out/bin/python3.12" --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath pythonLibs}"
+    '';
+  };
+
   packages' = with pkgs; [
-    # Base packages
-    python312
-  ] ++ lib.optionals isDevelopment [
-    # Development packages
+    wrappedPython
     poetry
     ruff
 
     (writeShellScriptBin "nixpkgs-update" ''
       set -e
-      hash=$(git ls-remote https://github.com/NixOS/nixpkgs nixpkgs-23.11-darwin | cut -f 1)
+      hash=$(git ls-remote https://github.com/NixOS/nixpkgs nixpkgs-unstable | cut -f 1)
       sed -i -E "s|/nixpkgs/archive/[0-9a-f]{40}\.tar\.gz|/nixpkgs/archive/$hash.tar.gz|" shell.nix
       echo "Nixpkgs updated to $hash"
     '')
   ];
 
-  shell' = with pkgs; lib.optionalString isDevelopment ''
-    [ ! -e .venv/bin/python ] && [ -h .venv/bin/python ] && rm -r .venv
+  shell' = with pkgs; ''
+    current_python=$(readlink -e .venv/bin/python || echo "")
+    current_python=''${current_python%/bin/*}
+    [ "$current_python" != "${wrappedPython}" ] && rm -r .venv
 
     echo "Installing Python dependencies"
     export POETRY_VIRTUALENVS_IN_PROJECT=1
+    poetry env use "${wrappedPython}/bin/python"
     poetry install --no-root --compile
 
     echo "Activating Python virtual environment"
     source .venv/bin/activate
 
-    export LD_LIBRARY_PATH="${lib.makeLibraryPath libraries'}"
-
     # Development environment variables
+    export PYTHONNOUSERSITE=1
+    export TZ=UTC
+
     if [ -f .env ]; then
       echo "Loading .env file"
       set -o allexport
       source .env set
-      +o allexport
+      set +o allexport
     fi
   '';
 in
 pkgs.mkShell {
-  libraries = libraries';
-  buildInputs = libraries' ++ packages';
+  buildInputs = packages';
   shellHook = shell';
 }
